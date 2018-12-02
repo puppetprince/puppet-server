a2aba3c0ee6a58c8a37ce67d077644b5dcdf98a1
Matt Griffin <matt@griffinonline.org>
2018-12-01 20:29:26 -0500

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+
+module.exports = {
+    entry: './assets/js/application.js',
+    output: {
+        path: path.resolve(__dirname, 'wwwroot/assets'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        modules: [path.resolve(__dirname, 'assets'), 'node_modules']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.elm$/,
+                exclude: [/elm-stuff/, /node_modules/],
+                use: {
+                    loader: 'elm-webpack-loader',
+                    options: {}
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: "style-loader/url" },
+                    { loader: "css-loader" }
+                ]
+            },
+            {
+                test: /\.scss$|\.sass$/,
+                use: [
+                    { loader: "style-loader/url" },
+                    { loader: "css-loader" },
+                    {
+                        loader: 'postcss-loader', // Run post css actions
+                        options: {
+                            plugins: function () { // post css plugins, can be exported to postcss.config.js
+                                return [
+                                    require('precss'),
+                                    require('autoprefixer')
+                                ];
+                            }
+                        }
+                    },
+                    { loader: "sass-loader" }
+                ]
+            }
+        ]
+    }
+};
